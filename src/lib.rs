8d2894208fcffc992de6031edaa80b9b23ec40dc
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2025-03-15 00:36:41 +0900

    :zap: add doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
-// 他のクレートから読み込む時はpublicにする必要がある
+//! ライブラリクレートのドキュメント（クレート全体のドキュメントになる）
+//! 他のクレートから読み込む時はpublicにする必要がある
+
+/// say_hello関数のドキュメント
 pub fn say_hello() {
     println!("Hello, world!");
+}
+
+/// **say_goodbye** 関数のドキュメント
+/// <p>Markdown形式でも書ける</p>
+/// ### 使い方
+/// ```
+/// fn main() {
+///     rust_practice::say_goodbye();
+/// }
+/// ```
+pub fn say_goodbye() {
+    println!("Goodbye");
 }
